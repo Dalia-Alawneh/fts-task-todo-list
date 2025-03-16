90ed17c2b2d0883e2c4a18f6c9615679de890fd4
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-03-16 13:41:20 +0200

    Create dark mode support

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+const THEME_KEY = 'theme'
+const themebtn = document.querySelector('.theme')
+const sunIcon = document.querySelector('.sun')
+const moonIcon = document.querySelector('.moon')
 
 const generateDate = () => {
   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -5,7 +9,7 @@ const generateDate = () => {
   const dayName = dayNames[date.getDay()];
   const options = { day: '2-digit', month: 'short', year: 'numeric' };
   const formattedDate = date.toLocaleDateString('en-GB', options);
-  
+
   return {
     date: formattedDate,
     dayName,
@@ -13,6 +17,26 @@ const generateDate = () => {
 }
 
 
-const {date, dayName} = generateDate()
+const { date, dayName } = generateDate()
 document.getElementById('date').innerHTML = date;
 document.getElementById('day').innerHTML = dayName;
+
+const toggleTheme = () => {
+  const html = document.documentElement;
+  html.classList.toggle("dark");
+  const isDarkMode = html.classList.contains('dark');
+  localStorage.setItem(THEME_KEY, isDarkMode ? 'dark' : 'light')
+
+  sunIcon.classList.toggle('hidden');
+  moonIcon.classList.toggle('hidden');
+}
+
+themebtn.onclick = toggleTheme;
+
+
+const theme = localStorage.getItem(THEME_KEY);
+if(theme === 'dark'){
+  document.documentElement.classList.add('dark')
+}else{
+  document.documentElement.classList.remove('dark')
+}
